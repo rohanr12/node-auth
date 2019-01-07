dc9cfb097f04cacdea98c658244cae05eeb98a23
rohanr12 <rohanst5000@gmail.com>
2019-01-07 09:50:40 +0530

    added basic views

--- app.js
+++ app.js
@@ -2,14 +2,31 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.set('view engine', 'pug');
+
 app.listen(8000, function(){
     console.log('listening');
 });
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.get('/register', function(req, res){
+    res.render('register');
+})
 
 app.post('/', function(req, res){
     var userName = req.body.name;
     var password = req.body.password;
     console.log(req.body);
 });
+
+
